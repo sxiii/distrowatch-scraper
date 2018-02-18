e06a79d105d820b959d61870e5e48166bbbc37be
Dennis Ivanov <securityxiii@gmail.com>
2018-02-19 01:21:47 +0300

    Create plotall.sh

---
+++ plotall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This script plots analytics based on gathered data.
+# It needs gnuplot to be installed along with 4 ".plot" files (templates).
+
+echo "Removing old png's..."
+
+rm *png
+
+echo "Generating new plots."
+
+gnuplot arch.plot
+gnuplot basedon.plot
+gnuplot status.plot
+gnuplot desktop.plot 
+convert -rotate 90 desktop.png desktop.png
+echo "Please plot the countries.png manually using other tools. It's too hard to make this image with gnuplot. You can use openheatmap.com"
+echo "Finished generating 4 PNG's."
